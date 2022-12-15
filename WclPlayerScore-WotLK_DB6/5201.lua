caae34e4e682bcaa804426fc2cc8cbd585726147
caca <icaca@live.com>
2022-12-15 17:48:29 +0800

    WCL Data Update

--- WclPlayerScore-WotLK_DB6/5201.lua
+++ WclPlayerScore-WotLK_DB6/5201.lua
@@ -1,10 +1,14 @@
 if(GetRealmName() == "魚人") then
 STOP_Database = {
+["有瑕疵丶"]="1痛苦术",
+["Poppy"]="1恶魔术",
+["疋杀地蔵"]="2痛苦术",
 
 }
 end
 if(GetRealmName() == "魚人") then
 WP_Database = {
+["有瑕疵丶"]="RX:(痛苦)920/52.9%|0",
 ["LASTUPDATE"]="2022-12-15"
 }
 end
