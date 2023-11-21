311bb723f923afa06508533f806536dfe1e62ebe
caca <icaca@live.com>
2023-11-22 07:48:10 +0800

    WCL Data Update

--- WclPlayerScore-WotLK_DB6/5072.lua
+++ WclPlayerScore-WotLK_DB6/5072.lua
@@ -6,6 +6,6 @@ STOP_Database = {
 }
 
 WP_Database = {
-["Tjaffe"]="CI:(防护)33796/7.3%RD:(审判)19198/70.2%|6",
-["LASTUPDATE"]="2023-11-21"
+["Tjaffe"]="CI:(防护)33796/7.3%RD:(审判)19198/70.2%|7",
+["LASTUPDATE"]="2023-11-22"
 }
