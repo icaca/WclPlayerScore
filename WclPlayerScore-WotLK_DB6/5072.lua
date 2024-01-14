2b88a538a563c18ebee53c30f16b4b7744476b03
caca <icaca@live.com>
2024-01-15 00:59:07 +0800

    WCL daily updates

--- WclPlayerScore-WotLK_DB6/5072.lua
+++ WclPlayerScore-WotLK_DB6/5072.lua
@@ -6,6 +6,6 @@ STOP_Database = {
 }
 
 WP_Database = {
-["Tjaffe"]="CI:(审判)30975/22.2%RD:(审判)19198/70.2%|1",
-["LASTUPDATE"]="2024-01-14"
+["Tjaffe"]="CI:(审判)31230/22.0%RD:(审判)19198/70.2%|1",
+["LASTUPDATE"]="2024-01-15"
 }
