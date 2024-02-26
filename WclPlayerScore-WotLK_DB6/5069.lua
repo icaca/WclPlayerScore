176f9621a9f1f123233f1fa157387f0d0b6bf993
caca <icaca@live.com>
2024-02-27 00:55:04 +0800

    WCL daily updates

--- WclPlayerScore-WotLK_DB6/5069.lua
+++ WclPlayerScore-WotLK_DB6/5069.lua
@@ -6,7 +6,7 @@ STOP_Database = {
 }
 
 WP_Database = {
-["Siopia"]="CI:(生存)19898/7.6%|6",
-["Lorefather"]="CI:(奥术)1558/10.3%|6",
-["LASTUPDATE"]="2024-02-26"
+["Siopia"]="CI:(生存)19898/7.6%|7",
+["Lorefather"]="CI:(奥术)1558/10.3%|7",
+["LASTUPDATE"]="2024-02-27"
 }
--- WclPlayerScore-WotLK_DB6/5072.lua
+++ WclPlayerScore-WotLK_DB6/5072.lua
@@ -6,6 +6,6 @@ STOP_Database = {
 }
 
 WP_Database = {
-["Tjaffe"]="CI:(审判)38316/19.5%|2",
-["LASTUPDATE"]="2024-02-26"
+["Tjaffe"]="CI:(审判)38316/19.5%|3",
+["LASTUPDATE"]="2024-02-27"
 }
