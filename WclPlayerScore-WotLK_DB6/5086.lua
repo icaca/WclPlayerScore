0c9b8157e74161e342802d444532f60b76b41477
caca <icaca@live.com>
2024-04-05 00:55:04 +0800

    WCL daily updates

--- WclPlayerScore-WotLK_DB6/5086.lua
+++ WclPlayerScore-WotLK_DB6/5086.lua
@@ -6,6 +6,6 @@ STOP_Database = {
 }
 
 WP_Database = {
-["Deadstoise"]="Er:(火焰)421/93.9%EI:(火焰)11663/85.3%ET:(奥术)3633/80.3%|1",
-["LASTUPDATE"]="2024-04-04"
+["Deadstoise"]="Er:(火焰)421/93.9%EI:(火焰)11663/85.3%ET:(奥术)3633/80.3%|2",
+["LASTUPDATE"]="2024-04-05"
 }
