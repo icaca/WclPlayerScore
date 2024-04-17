b8782f9e0526e3705cc1981261ebed0a227c1222
caca <icaca@live.com>
2024-04-17 08:43:36 +0800

    WCL daily updates

--- WclPlayerScore-WotLK_DB5/5077.lua
+++ WclPlayerScore-WotLK_DB5/5077.lua
@@ -6,5 +6,29 @@ STOP_Database = {
 }
 
 WP_Database = {
-["LASTUPDATE"]="2024-04-16"
+["Eshqawfirst"]="AI:(符文)1/0AT:(鲜血)1/0|1",
+["Borman"]="AI:(冰霜)1/0AT:(冰霜)1/0|1",
+["Redanskaya"]="AI:(平衡)1/0AT:(平衡)1/0|1",
+["Eléu"]="AI:(野性)1/0AT:(野性)1/0|1",
+["Terebonka"]="AI:(生存)1/0AT:(生存)1/0|1",
+["Airmj"]="AI:(生存)2/0AT:(生存)2/0|1",
+["Anfa"]="AI:(火焰)1/0AT:(火焰)1/0|1",
+["Emidiuse"]="AI:(神圣)2/0AT:(神圣)2/0|1",
+["Primeouter"]="AI:(神圣)1/0AT:(神圣)1/0|1",
+["Frezerr"]="AI:(防护)1/0AT:(防护)1/0|1",
+["Chronicaa"]="AI:(惩戒)3/0AT:(惩戒)2/0|1",
+["Tenelom"]="AI:(惩戒)1/0AT:(惩戒)1/0|1",
+["Arutor"]="AI:(惩戒)2/0AT:(惩戒)3/0|1",
+["Whités"]="AI:(戒律)1/0AT:(戒律)1/0|1",
+["Afkunicorn"]="AI:(暗影)1/0AT:(暗影)1/0|1",
+["Fý"]="AI:(暗影)2/0AT:(暗影)2/0|1",
+["Phantomblade"]="AI:(战斗)1/0AT:(战斗)1/0|1",
+["Djon"]="AI:(战斗)2/0AT:(战斗)2/0|1",
+["Hinkalya"]="AI:(恢复)2/0AT:(恢复)2/0|1",
+["Drudder"]="AI:(恢复)1/0AT:(恢复)1/0|1",
+["Nintendopro"]="AI:(痛苦)1/0AT:(痛苦)1/0|1",
+["Tôxin"]="AI:(恶魔)1/0AT:(恶魔)1/0|1",
+["Advokat"]="AI:(狂怒)1/0AT:(狂怒)2/0|1",
+["Faq"]="AI:(狂怒)2/0AT:(狂怒)1/0|1",
+["LASTUPDATE"]="2024-04-17"
 }
